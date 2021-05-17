bb95b546ba9205c499cb2687b3038ff399a7a35b
Nicolas Rose <nickfbp@gmail.com>
2021-05-17 14:10:10 -0600

    Updated Components - Navbar/Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
+import Banner from './Component/Banner';
+import About from './Component/About';
+import Service from './Component/Service';
+import PricingPlan from './Component/PricingPlan';
+import Blog from './Component/Blog';
+import Contact from './Component/Contact';
+import Footer from './Component/Footer';
+import Testimonial from './Component/Testimonial';
+import Portfolio from './Component/Portfolio';
+import Navbar from './Component/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './Assets/Css/Responsive.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <Banner/>
+            <About/>
+            <Service/>
+            <Portfolio/>
+            <Testimonial/>
+            <PricingPlan/>
+            <Blog/>
+            <Contact/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
